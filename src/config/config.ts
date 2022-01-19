26838f0a2bf7468c9be9a33b30c3c04771d818f0
hedgehog <imhedgehog@qq.com>
2022-01-20 00:56:01 +0800

    fix: 修正环境变量名

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@
 
 const Config = {
   // API请求的baseUrl
-  BASE_URL: process.env.REACT_APP_PUBLISH_PATH,
+  BASE_URL: process.env.REACT_APP_PUBLIC_PATH,
   PROJECT_NAME: process.env.REACT_APP_PROJECT_NAME
 }
 
