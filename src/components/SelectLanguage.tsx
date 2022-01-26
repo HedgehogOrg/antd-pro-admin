87612750a0c93adb97bdfc32d4534578574c5ecc
hedgehog <imhedgehog@qq.com>
2022-01-26 15:09:23 +0800

    perf: 高亮当前语言

--- src/components/SelectLanguage.tsx
+++ src/components/SelectLanguage.tsx
@@ -5,7 +5,7 @@ import Lang from '../locales/index';
 type Props = {};
 
 const SelectLanguage = (props: Props) => {
-  const LanguageList = <Menu>
+  const LanguageList = <Menu selectedKeys={[user.language]}>
    {
      Lang.languages.map((item) => (
        <Menu.Item key={item.value} onClick={() => user.setLanguage(item.value)}>{ item.name }</Menu.Item>
