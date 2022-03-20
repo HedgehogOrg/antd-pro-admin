5929cab4858fa322704a9bc1fcc9fd0870a057c3
hedgehog <imhedgehog@qq.com>
2022-03-21 01:05:38 +0800

    perf: all class components to function components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import React, { Component } from "react";
 import './App.css'
 import { Outlet } from 'react-router-dom';
 import { observer } from "mobx-react";
 
-interface Props {}
-interface State {}
-
-
-@observer
-class App extends Component<Props, State> {
-  state = {};
-
-  render() {
-    return <Outlet />;
-  }
+const App = () => {
+  return <Outlet />;
 }
 
-export default App;
+export default observer(App);
--- src/modules/dashboard/pages/Dashboard.tsx
+++ src/modules/dashboard/pages/Dashboard.tsx
@@ -1,29 +1,17 @@
-import React, { Component } from 'react'
 import ChartsArea from '../components/ChartsArea';
 import { observer } from 'mobx-react';
 import user from '../../../stores/user';
 import { Link } from 'react-router-dom';
 
-interface Props {
-
-}
-interface State {
-
-}
-
-@observer
-class Dashboard extends Component<Props, State> {
-
-  render() {
-    return (
-      <div>
-        dashboard
-        user: {JSON.stringify(user.user)}
-        <Link to={'/404'}>404</Link>
-        <ChartsArea></ChartsArea>
-      </div>
-    )
-  }
+const Dashboard = () => {
+  return (
+    <div>
+      dashboard
+      user: {JSON.stringify(user.user)}
+      <Link to={'/404'}>404</Link>
+      <ChartsArea></ChartsArea>
+    </div>
+  )
 }
 
-export default Dashboard
+export default observer(Dashboard)
